a90e546b56f81bef421159fe788a9a8c06bde3b3
Farion Oleksandr <sashafarion@gmail.com>
2024-04-02 00:01:14 +0300

    added fixes to the checkstyle

--- src/main/java/com/farion/onlinebookstore/Application.java
+++ src/main/java/com/farion/onlinebookstore/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineBookStoreApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineBookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/test/java/com/farion/onlinebookstore/ApplicationTests.java
+++ src/test/java/com/farion/onlinebookstore/ApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineBookStoreApplicationTestsDto {
-	@Test
-	void contextLoads() {
-	}
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
