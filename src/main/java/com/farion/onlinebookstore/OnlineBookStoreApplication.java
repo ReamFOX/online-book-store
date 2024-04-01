db85854f9f11eb26672d721557b5d54a5f0a8a30
Farion Oleksandr <sashafarion@gmail.com>
2024-04-01 20:57:23 +0300

    ***********

--- src/main/java/com/farion/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/farion/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,14 +1,10 @@
 package com.farion.onlinebookstore;
 
-import com.farion.onlinebookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-	@Autowired
-	private BookService bookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookStoreApplication.class, args);
