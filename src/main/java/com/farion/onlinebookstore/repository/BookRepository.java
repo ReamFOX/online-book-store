3b8b0b19ca1528eecfc1dff9bb3e66c080da0966
Farion Oleksandr <sashafarion@gmail.com>
2024-04-02 17:28:57 +0300

    Replaced the existing BookRepository with the one that use JpaRepository interface

--- src/main/java/com/farion/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/farion/onlinebookstore/repository/BookRepository.java
@@ -3,8 +3,9 @@ package com.farion.onlinebookstore.repository;
 import com.farion.onlinebookstore.entity.Book;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book save(Book book);
 
     Optional<Book> findById(Long id);
