4831a5184e80dcfb0007ace298a3b64d0f7552e1
Farion Oleksandr <sashafarion@gmail.com>
2024-04-04 13:10:07 +0300

    added CustomGlobalExceptionHandler.java

--- src/main/java/com/farion/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/farion/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,22 +1,30 @@
 package com.farion.onlinebookstore.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotEmpty
+    @Size(max = 255, message = "can`t be longer than 255 characters")
     private String title;
-    @NotNull
+    @NotEmpty
+    @Size(max = 255, message = "can`t be longer than 255 characters")
     private String author;
-    @ISBN
+    @ISBN(message = "invalid format")
     private String isbn;
     @NotNull
-    @Min(0)
+    @DecimalMin(value = "0.00", inclusive = false, message = "can't be 0 or less")
     private BigDecimal price;
+    @Size(max = 255, message = "can`t be longer than 255 characters")
     private String description;
+    @URL(message = "invalid url")
+    @Size(max = 255, message = "can`t be longer than 255 characters")
     private String coverImage;
 }
