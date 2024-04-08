d72b2c402c8b007aab75c5f721e6dc204ee0292d
Farion Oleksandr <sashafarion@gmail.com>
2024-04-08 18:27:51 +0300

    added fixes

---
+++ src/main/java/com/farion/onlinebookstore/repository/role/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.farion.onlinebookstore.repository.role;
+
+import com.farion.onlinebookstore.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
