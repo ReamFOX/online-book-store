3bb643536ed2fc66a344d6c5d2f467b0eb84d8aa
Farion Oleksandr <sashafarion@gmail.com>
2024-04-08 12:43:59 +0300

    added entity and liquibase script

---
+++ src/main/java/com/farion/onlinebookstore/entity/User.java
@@ -0,0 +1,24 @@
+package com.farion.onlinebookstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+}
