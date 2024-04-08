fa3f69962e2dc7074269a9c8b98c0cd8b0f84918
Farion Oleksandr <sashafarion@gmail.com>
2024-04-08 17:45:30 +0300

    added liquibase scripts, safe delete for User and Role entity

---
+++ src/main/java/com/farion/onlinebookstore/entity/Role.java
@@ -0,0 +1,26 @@
+package com.farion.onlinebookstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+}
+
+enum RoleName {
+    ADMIN, USER
+}
+
+
--- src/main/java/com/farion/onlinebookstore/entity/User.java
+++ src/main/java/com/farion/onlinebookstore/entity/User.java
@@ -2,14 +2,21 @@ package com.farion.onlinebookstore.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.Set;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
 @Table(name = "users")
 public class User {
     @Id
@@ -19,10 +26,13 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
-    @Column(name = "first_name", nullable = false)
+    @Column(nullable = false)
     private String firstName;
-    @Column(name = "last_name", nullable = false)
+    @Column(nullable = false)
     private String lastName;
-    @Column(name = "shipping_address")
     private String shippingAddress;
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Role> roles;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
