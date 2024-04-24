e61505aa799e5b4b8804229d51e95397bfb4ad5b
Farion Oleksandr <sashafarion@gmail.com>
2024-04-24 21:14:45 +0300

    resolved conflicts

--- src/main/java/com/farion/onlinebookstore/entity/User.java
+++ src/main/java/com/farion/onlinebookstore/entity/User.java
@@ -11,15 +11,14 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
@@ -54,10 +53,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-                .map(role ->
-                        new SimpleGrantedAuthority(role.getAuthority()))
-                .collect(Collectors.toSet());
+        return new HashSet<>(roles);
     }
 
     @Override
